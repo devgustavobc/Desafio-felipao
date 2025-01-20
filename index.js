8c96d8f3867fcdb1a79fe0bd1bc876a1bfe0aeb3
devgustavobc <devgustavobc@gmail.com>
2025-01-20 20:25:52 -0300

    codigo do desafio feito e funcionando

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("Hello World");
+//Bem redundante mas funciona kkkkkkk :D
+
+let experiencia = 12000;
+
+if (experiencia <= 1000) {
+  experiencia = "Ferro";
+  console.log("O Herói de nome Guti está no nível de " + experiencia);
+} else if (experiencia >= 1001 && experiencia < 2000) {
+  experiencia = "Bronze";
+  console.log("O Herói de nome Guti está no nível de " + experiencia);
+} else if (experiencia >= 2001 && experiencia < 5000) {
+  experiencia = "Prata";
+  console.log("O Herói de nome Guti está no nível de " + experiencia);
+} else if (experiencia >= 5001 && experiencia < 7000) {
+  experiencia = "Ouro";
+  console.log("O Herói de nome Guti está no nível de " + experiencia);
+} else if (experiencia >= 7001 && experiencia < 8000) {
+  experiencia = "Platina";
+  console.log("O Herói de nome Guti está no nível de " + experiencia);
+} else if (experiencia >= 8001 && experiencia < 9000) {
+  experiencia = "Ascendente";
+  console.log("O Herói de nome Guti está no nível de " + experiencia);
+} else if (experiencia >= 9001 && experiencia < 10000) {
+  experiencia = "Imortal";
+  console.log("O Herói de nome Guti está no nível de " + experiencia);
+} else if (experiencia >= 10001) {
+  experiencia = "Radiante";
+  console.log("O Herói de nome Guti está no nível de " + experiencia);
+}
