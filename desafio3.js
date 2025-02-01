d42baa0102cf23edb3aefab0bb05b2c8b269f67f
devgustavobc <devgustavobc@gmail.com>
2025-02-01 01:12:51 -0300

    desafio concluido

---
+++ desafio3.js
@@ -0,0 +1,36 @@
+class hero{
+    constructor(name, age, type){
+        this.name = name
+        this.age = age
+        this.type = type
+    }
+
+    attack(){
+        let ataque;
+
+        if (this.type == "mago")
+            ataque = 'magia'
+
+        else if (this.type == "guerreiro")
+            ataque = 'espada'
+
+        else if (this.type == "monge")
+            ataque = 'artes marciais'
+
+        else if (this.type == "ninja")
+            ataque = 'shuriken'
+
+        console.log(`o ${this.type} atacou usando ${ataque}`)
+
+    }   
+}
+
+let newHero = new hero("Varian Wrynn", "45", "guerreiro")
+let newHeroTwo = new hero("Jaina Proudmoore", "45", "mago")
+let newHeroThree = new hero("Taran Zhu", "80", "monge")
+let newHeroFour = new hero("Edwin VanCleef", "35", "ninja")
+
+newHero.attack()
+newHeroTwo.attack()
+newHeroThree.attack()
+newHeroFour.attack()
